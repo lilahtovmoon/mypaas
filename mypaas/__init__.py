597cff158dc8be769bbc96813873eaffc7ff164c
Lilah Abigail Ruth <lilahtovmoon@icloud.com>
2021-02-07 16:11:09 -0500

    downgrading to 2.1.6

--- mypaas/__init__.py
+++ mypaas/__init__.py
@@ -8,7 +8,7 @@ automatic https (via Let's Encrypt) and deployments via dockerfiles.
 
 
 __version__ = "0.5.1"
-__traefik_version__ = "2.1.9"
+__traefik_version__ = "2.1.6"
 
 version_info = tuple(map(int, __version__.split(".")))
 
